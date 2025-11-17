daba33087bb0b326569fafe8657922839feb9ded
--global <guduruvinay3@gmail.com>
2025-11-17 11:04:57 +0530

    Added Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header(){
+    return (
+        <header>
+            <h1>Simple To-Do Application</h1>
+            <p>Add | Edit | Complete | Delete</p>
+        </header>
+    )
+}
+
+export default Header
